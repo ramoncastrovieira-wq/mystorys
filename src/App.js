7239a26870fa3e7b6999a19695200356c6169b17
Ramon <ramoncastrovieira@gmail.com>
2026-02-02 20:05:56 -0300

    Segunda versão

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Router/>
+      <Router>
+      </Router>
     </div>
   );
 }
--- src/Pages/Router/Router.jsx
+++ src/Pages/Router/Router.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import HomePage from "../HomePage/HomePage";
+import Profile from "../Profile/Profile";
 
 const Router = () => {
     return (
-        <div>Router</div>
-    )
-}
+        <div>
+            <div className="flex">
+                <div className="w-[20%] border border-1-slate-500 ">
+                    <Sidebar />
+                </div>
+            <div>
+                    <Routes>
+                        <Route path="/" element={<HomePage />}></Route>
+                        <Route path="/username" element={<Profile />}></Route>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Router
