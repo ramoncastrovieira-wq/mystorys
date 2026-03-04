bbbd74b78f218a61b20749a853a4a81e938ef68f
Ramon <ramoncastrovieira@gmail.com>
2026-03-03 21:15:12 -0300

    Modificação no postcard

--- src/Components/Post/PostCard.jsx
+++ src/Components/Post/PostCard.jsx
@@ -1,25 +1,80 @@
-import React from 'react'
-import { BsThreeDots } from 'react-icons/bs';
+import React, { useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { FaRegComment } from "react-icons/fa";
+import { RiSendPlaneLine } from "react-icons/ri";
+import "./PostCard.css";
+
 const PostCard = () => {
-    const [showDropDown, setShowDropdown]=useState(false);
-    const handleClick=()=>{
-setShowDropdown(!showDropDown);
-    }
-    return (
+  const [showDropDown, setShowDropdown] = useState(false);
+  const [isPostLiked, setIsPostLiked] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const handleSavePost = () => {
+    setIsSaved(!isSaved);
+  };
+
+  const handlePostLike = () => {
+    setIsPostLiked(!isPostLiked);
+  };
+
+  const handleClick = () => {
+    setShowDropdown(!showDropDown);
+  };
+
+  return (
     <div>
-        <div className="border rounded-md w-full">
-            <div className="flex justify-between items-center w-full py-4 px-5">
-            <div className="flex items-center">
-                        </div>
-                        
-                        </div>
-                            <BsThreeDots onClick={handleClick}/>
-                        <div onClick={handleClick} className="dropdown-content">
-                        {showDropDown && <p className='bg-black text-white py-1 px-4 rounded-md cursor-pointer'>Delete</p>}
-                    </div>
-                </div>
+      <div className="border rounded-md w-full">
+        <div className="flex justify-between items-center w-full py-4 px-5">
+        </div>
+
+        <div className="flex justify-between items-center w-full px-5 py-4">
+          <div className="flex items-center space-x-2">
+            {isPostLiked ? (
+              <AiFillHeart
+                className="text-2xl hover:opacity-50 cursor-pointer text-red-500"
+                onClick={handlePostLike}
+              />
+            ) : (
+              <AiOutlineHeart
+                className="text-2xl hover:opacity-50 cursor-pointer"
+                onClick={handlePostLike}
+              />
+            )}
+
+            <FaRegComment className="text-xl hover:opacity-50 cursor-pointer" />
+            <RiSendPlaneLine className="text-xl hover:opacity-50 cursor-pointer" />
+          </div>
+
+          <div className="cursor-pointer">
+            {isSaved ? (
+              <BsBookmarkFill
+                onClick={handleSavePost}
+                className="text-xl hover:opacity-50 cursor-pointer"
+              />
+            ) : (
+              <BsBookmark
+                onClick={handleSavePost}
+                className="text-xl hover:opacity-50 cursor-pointer"
+              />
+            )}
+          </div>
+        </div>
+
+        <div className="w-full py-2 px-5">
+            <p>10 likes</p>
+            <p className="opacity-50 py-2 cursor-pointer">view all 10 comments</p>
+        </div>
+
+        <div>
+            <div className="flex w-full items-center px-5">
+                <BsEmojiSmile/>
+                <input type="text" placeholder="Add a comment..." className="w-full border-t py-2 px-5 focus:outline-none" />
             </div>
-    );
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default PostCard
+export default PostCard;
