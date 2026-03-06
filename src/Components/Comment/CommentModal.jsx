9993a9cd14a29c29d888d38e69052e780087da9e
Ramon <ramoncastrovieira@gmail.com>
2026-03-06 16:32:22 -0300

    A lot of little changes using chakra ui

--- src/Components/Comment/CommentModal.jsx
+++ src/Components/Comment/CommentModal.jsx
@@ -1,8 +1,30 @@
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
 import React from 'react'
 
-const CommentModal = () => {
+const CommentModal = (onClose, isOpen) => {
     return (
-        <div>Comment</div>
+        <div>
+            <Modal size={""} onClose={onClose} isOpen={isOpen} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+
+
+                    <ModalBody>
+                    <div>
+                        <div className='w- [45%] flex flex-col justify-center'>
+                        <img className='max-h-full w-full' src="https://cdn.pixabay.com/photo/2023/03/17/10/44/sunset-7858360__340.jpg" alt=""/>
+                    </div>
+                        <div>
+                        </div>
+                    </div>
+                    <div className="border w-55%">
+
+                        </div>
+                    </ModalBody>
+
+                </ModalContent>
+            </Modal>
+        </div>
     )
 }
 
--- src/Components/Post/PostCard.jsx
+++ src/Components/Post/PostCard.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { BsBookmark, BsBookmarkFill, BsEmojiSmile } from "react-icons/bs";
 import { FaRegComment } from "react-icons/fa";
 import { RiSendPlaneLine } from "react-icons/ri";
 import "./PostCard.css";
 
 const PostCard = () => {
-  const [showDropDown, setShowDropdown] = useState(false);
   const [isPostLiked, setIsPostLiked] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
 
@@ -18,61 +17,65 @@ const PostCard = () => {
     setIsPostLiked(!isPostLiked);
   };
 
-  const handleClick = () => {
-    setShowDropdown(!showDropDown);
-  };
-
   return (
-    <div>
-      <div className="border rounded-md w-full">
-        <div className="flex justify-between items-center w-full py-4 px-5">
+    <div className="border rounded-md w-full">
+      
+      <div className="flex justify-between items-center w-full py-4 px-5">
+      </div>
+
+      <div className="flex justify-between items-center w-full px-5 py-4">
+        <div className="flex items-center space-x-2">
+          
+          {isPostLiked ? (
+            <AiFillHeart
+              className="text-2xl hover:opacity-50 cursor-pointer text-red-500"
+              onClick={handlePostLike}
+            />
+          ) : (
+            <AiOutlineHeart
+              className="text-2xl hover:opacity-50 cursor-pointer"
+              onClick={handlePostLike}
+            />
+          )}
+
+          <FaRegComment className="text-xl hover:opacity-50 cursor-pointer" />
+          <RiSendPlaneLine className="text-xl hover:opacity-50 cursor-pointer" />
+
         </div>
 
-        <div className="flex justify-between items-center w-full px-5 py-4">
-          <div className="flex items-center space-x-2">
-            {isPostLiked ? (
-              <AiFillHeart
-                className="text-2xl hover:opacity-50 cursor-pointer text-red-500"
-                onClick={handlePostLike}
-              />
-            ) : (
-              <AiOutlineHeart
-                className="text-2xl hover:opacity-50 cursor-pointer"
-                onClick={handlePostLike}
-              />
-            )}
-
-            <FaRegComment className="text-xl hover:opacity-50 cursor-pointer" />
-            <RiSendPlaneLine className="text-xl hover:opacity-50 cursor-pointer" />
-          </div>
-
-          <div className="cursor-pointer">
-            {isSaved ? (
-              <BsBookmarkFill
-                onClick={handleSavePost}
-                className="text-xl hover:opacity-50 cursor-pointer"
-              />
-            ) : (
-              <BsBookmark
-                onClick={handleSavePost}
-                className="text-xl hover:opacity-50 cursor-pointer"
-              />
-            )}
-          </div>
-        </div>
-
-        <div className="w-full py-2 px-5">
-            <p>10 likes</p>
-            <p className="opacity-50 py-2 cursor-pointer">view all 10 comments</p>
-        </div>
-
-        <div>
-            <div className="flex w-full items-center px-5">
-                <BsEmojiSmile/>
-                <input type="text" placeholder="Add a comment..." className="w-full border-t py-2 px-5 focus:outline-none" />
-            </div>
+        <div className="cursor-pointer">
+          {isSaved ? (
+            <BsBookmarkFill
+              onClick={handleSavePost}
+              className="text-xl hover:opacity-50"
+            />
+          ) : (
+            <BsBookmark
+              onClick={handleSavePost}
+              className="text-xl hover:opacity-50"
+            />
+          )}
         </div>
       </div>
+
+      <div className="w-full py-2 px-5">
+        <p>10 likes</p>
+        <p className="opacity-50 py-2 cursor-pointer">
+          view all 10 comments
+        </p>
+      </div>
+
+      <div className="border-t w-full">
+        <div className="flex w-full items-center px-5 py-3 space-x-2">
+          <BsEmojiSmile />
+          <input
+            className="commentInput flex-grow outline-none"
+            type="text"
+            placeholder="Add a comment..."
+          />
+        </div>
+      </div>
+
     </div>
   );
 };
